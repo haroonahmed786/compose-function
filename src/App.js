0c11c8376ebec581894d73211864281c89d5034c
haroonahmed786 <Haroon65ahmed@gmail.com>
2022-09-24 13:24:50 -0700

    compose-function

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"; 
+import Student from "./Student";
+
+const App =()=>{
+    return(
+   <div>
+    <Student name="Haroon"/>
+    <Student name="Waqas" />
+    <Student name="Ali" />
+    <Student name="Usman" />
+   </div>
+    );
+};
+export default App;
